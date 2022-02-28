5bd62994e56c4ad42af4c24284fc8c62988c425b
Felipe Gabriel Arend <fga.felipe@gmail.com>
2022-02-28 16:03:35 -0300

    Move mock of fetch random advice to a helper function

--- src/App.test.js
+++ src/App.test.js
@@ -6,35 +6,32 @@ beforeEach(() => {
   jest.clearAllMocks();
 });
 
-test("renders a random advice", async () => {
-  jest
+function mockFetchRandomAdvice(advice = "any_random_advice") {
+  return jest
     .spyOn(global, "fetch")
     .mockName("fetch")
     .mockImplementationOnce(() =>
       Promise.resolve({
         json: jest.fn().mockResolvedValueOnce({
           slip: {
-            advice: "any_random_advice",
-          }
+            advice,
+          },
         }),
       })
     );
+}
+
+test("renders a random advice", async () => {
+  mockFetchRandomAdvice("a_random_advice");
 
   render(<App />);
-  const adviceElement = await screen.findByText(/^any_random_advice$/);
+  const adviceElement = await screen.findByText(/^a_random_advice$/);
 
   expect(adviceElement).toBeInTheDocument();
 });
 
 test("calls advice slip API for a random advice", () => {
-  const fetchSpy = jest
-    .spyOn(global, "fetch")
-    .mockName("fetch")
-    .mockImplementationOnce(() =>
-      Promise.resolve({
-        json: jest.fn(),
-      })
-    );
+  const fetchSpy = mockFetchRandomAdvice();
 
   render(<App />);
 
