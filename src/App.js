23ce5cef827127bb3c3c7ed35aa17f0cfc3864a5
Felipe Gabriel Arend <fga.felipe@gmail.com>
2022-02-28 15:34:17 -0300

    App calls advice slip API for a random advice

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
+import { useEffect } from "react";
+
 import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      Hello World!
-    </div>
-  );
+  useEffect(() => {
+    async function fetchRandomAdvice() {
+      await fetch("https://api.adviceslip.com/advice");
+    }
+
+    fetchRandomAdvice();
+  });
+
+  return <div className="App">Hello World!</div>;
 }
 
 export default App;
--- src/App.test.js
+++ src/App.test.js
@@ -2,9 +2,28 @@ import { render, screen } from "@testing-library/react";
 
 import App from "./App";
 
+beforeEach(() => {
+  jest.clearAllMocks();
+});
+
 test("renders hello world", () => {
   render(<App />);
   const helloElement = screen.getByText(/hello world!/i);
 
   expect(helloElement).toBeInTheDocument();
+});
+
+test("calls advice slip API for a random advice", () => {
+  const fetchSpy = jest
+    .spyOn(global, "fetch")
+    .mockName("fetch")
+    .mockImplementationOnce(() =>
+      Promise.resolve({
+        json: jest.fn(),
+      })
+    );
+
+  render(<App />);
+
+  expect(fetchSpy).toHaveBeenCalledWith("https://api.adviceslip.com/advice");
 });
