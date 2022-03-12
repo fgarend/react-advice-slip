3e0660ac8a203663317168b84674bef5b449fafc
Felipe Gabriel Arend <fga.felipe@gmail.com>
2022-03-12 09:39:19 -0300

    Improve Header tests

--- src/Header/Header.test.jsx
+++ src/Header/Header.test.jsx
@@ -3,10 +3,11 @@ import { render, screen } from "@testing-library/react";
 import Header from "./Header";
 
 describe("App header", () => {
-  test("renders an advice slip header", async () => {
+  test("renders 'advice slip' heading", async () => {
     render(<Header />);
 
-    const header = screen.getByRole("heading", { level: 1, name: /^Advice Slip$/ });
-    expect(header).toBeInTheDocument();
+    const header = screen.getByRole("heading", { level: 1 });
+
+    expect(header).toHaveTextContent(/^Advice Slip$/);
   });
 });
